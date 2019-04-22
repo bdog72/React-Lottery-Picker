68967dbbf8e8fb10e2aa3aab2e7c99e82103b04a
Brian Bycynski <high5bri@hotmail.com>
2019-04-22 10:09:53 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import Lottery from './Lottery';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Lottery />
+        <Lottery title="Mini Daily" maxNum={10} maxBalls={4} />
       </div>
     );
   }
